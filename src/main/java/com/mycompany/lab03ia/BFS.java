9a891cee8c957ecff03312ee4718e203b7a0e9fe
genecc15 <36336252+genecc15@users.noreply.github.com>
2021-02-08 22:59:15 -0600

    PrimerCommit

---
+++ src/main/java/com/mycompany/lab03ia/BFS.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab03ia;
+
+/**
+ *
+ * @author Génesis Cortez
+ */
+public class BFS {
+    
+}
